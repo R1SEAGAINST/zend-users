d9046d7b132231b9e3fdd1f9a35598603baba76d
r153against <bgeorgiev1990@gmail.com>
2017-06-22 17:56:51 +0300

    database

--- module/User/src/Entity/User.php
+++ module/User/src/Entity/User.php
@@ -3,146 +3,224 @@ namespace User\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-
-class User 
+/**
+ * This class represents a registered user.
+ * @ORM\Entity()
+ * @ORM\Table(name="user")
+ */
+class User
 {
     // User status constants.
     const STATUS_ACTIVE       = 1; // Active user.
     const STATUS_RETIRED      = 2; // Retired user.
-    
 
+    /**
+     * @ORM\Id
+     * @ORM\Column(name="id")
+     * @ORM\GeneratedValue
+     */
     protected $id;
 
-
+    /**
+     * @ORM\Column(name="email")
+     */
     protected $email;
-    
 
+    /**
+     * @ORM\Column(name="full_name")
+     */
     protected $fullName;
 
-
+    /**
+     * @ORM\Column(name="password")
+     */
     protected $password;
 
-
+    /**
+     * @ORM\Column(name="status")
+     */
     protected $status;
-    
 
+    /**
+     * @ORM\Column(name="date_created")
+     */
     protected $dateCreated;
 
+    /**
+     * @ORM\Column(name="pwd_reset_token")
+     */
     protected $passwordResetToken;
-    
 
+    /**
+     * @ORM\Column(name="pwd_reset_token_creation_date")
+     */
     protected $passwordResetTokenCreationDate;
-    
 
-    public function getId() 
+    /**
+     * Returns user ID.
+     * @return integer
+     */
+    public function getId()
     {
         return $this->id;
     }
 
-
-    public function setId($id) 
+    /**
+     * Sets user ID.
+     * @param int $id
+     */
+    public function setId($id)
     {
         $this->id = $id;
     }
 
-
-    public function getEmail() 
+    /**
+     * Returns email.
+     * @return string
+     */
+    public function getEmail()
     {
         return $this->email;
     }
 
-
-    public function setEmail($email) 
+    /**
+     * Sets email.
+     * @param string $email
+     */
+    public function setEmail($email)
     {
         $this->email = $email;
     }
-    
 
-    public function getFullName() 
+    /**
+     * Returns full name.
+     * @return string
+     */
+    public function getFullName()
     {
         return $this->fullName;
-    }       
+    }
 
-
-    public function setFullName($fullName) 
+    /**
+     * Sets full name.
+     * @param string $fullName
+     */
+    public function setFullName($fullName)
     {
         $this->fullName = $fullName;
     }
-    
 
-    public function getStatus() 
+    /**
+     * Returns status.
+     * @return int
+     */
+    public function getStatus()
     {
         return $this->status;
     }
 
-
-    public static function getStatusList() 
+    /**
+     * Returns possible statuses as array.
+     * @return array
+     */
+    public static function getStatusList()
     {
         return [
             self::STATUS_ACTIVE => 'Active',
             self::STATUS_RETIRED => 'Retired'
         ];
-    }    
-    
+    }
 
+    /**
+     * Returns user status as string.
+     * @return string
+     */
     public function getStatusAsString()
     {
         $list = self::getStatusList();
         if (isset($list[$this->status]))
             return $list[$this->status];
-        
-        return 'Unknown';
-    }    
-    
 
-    public function setStatus($status) 
+        return 'Unknown';
+    }
+
+    /**
+     * Sets status.
+     * @param int $status
+     */
+    public function setStatus($status)
     {
         $this->status = $status;
-    }   
-    
-
-    public function getPassword() 
-    {
-       return $this->password; 
     }
-    
 
-    public function setPassword($password) 
+    /**
+     * Returns password.
+     * @return string
+     */
+    public function getPassword()
+    {
+       return $this->password;
+    }
+
+    /**
+     * Sets password.
+     * @param string $password
+     */
+    public function setPassword($password)
     {
         $this->password = $password;
     }
-    
 
-    public function getDateCreated() 
+    /**
+     * Returns the date of user creation.
+     * @return string
+     */
+    public function getDateCreated()
     {
         return $this->dateCreated;
     }
-    
 
-    public function setDateCreated($dateCreated) 
+    /**
+     * Sets the date when this user was created.
+     * @param string $dateCreated
+     */
+    public function setDateCreated($dateCreated)
     {
         $this->dateCreated = $dateCreated;
-    }    
-    
+    }
 
+    /**
+     * Returns password reset token.
+     * @return string
+     */
     public function getResetPasswordToken()
     {
         return $this->passwordResetToken;
     }
-    
 
-    public function setPasswordResetToken($token) 
+    /**
+     * Sets password reset token.
+     * @param string $token
+     */
+    public function setPasswordResetToken($token)
     {
         $this->passwordResetToken = $token;
     }
 
+    /**
+     * Returns password reset token's creation date.
+     * @return string
+     */
     public function getPasswordResetTokenCreationDate()
     {
         return $this->passwordResetTokenCreationDate;
     }
-    
 
-    public function setPasswordResetTokenCreationDate($date) 
+    /**
+     * Sets password reset token's creation date.
+     * @param string $date
+     */
+    public function setPasswordResetTokenCreationDate($date)
     {
         $this->passwordResetTokenCreationDate = $date;
     }
